9f478b3c3cf949f0448036ef2e4ceb5fd4420c12
NoahReaver <emilianojaparicio@gmail.com>
2022-07-16 14:41:55 -0300

    BackTesting

--- api/src/routes/activities.js
+++ api/src/routes/activities.js
@@ -1,7 +1,24 @@
 const { Router } = require("express");
-const { Country } = require("../models/Country");
+const { Activity } = require("../db");
 const router = Router();
 
+router.post("/", async (req, res) => {
+  const { name, difficulty, duration, season } = req.body;
 
+  if (!name) return res.status(404).send("La actividad debe tener un nombre");
+
+  try {
+    const activity = await Activity.create({
+      name,
+      difficulty,
+      duration,
+      season,
+    });
+
+    return res.status(201).json(activity.toJSON());
+  } catch (e) {
+    return res.status(404).send("Error en los datos de la actividad");
+  }
+});
 
 module.exports = router;
--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -1,81 +1,80 @@
 const { Router } = require("express");
-const { Country } = require("../db");
+const { Op, Country, Activity } = require("../db");
+const axios = require("axios");
 const router = Router();
 
-let storedData = false;
+
 
 function validateStoredData(req, res, next) {
+  try {
+    let storedData = Country.findByPk("ARG");
+  } catch (e) {
+    
+  }
   if (!storedData) {
-    fetch("https://restcountries.com/v3/all")
-      .then((response) => response.json())
-      .then((json) => {
-        const countriesDB = json.map((c) => {
-          return {
-            id: c.fifa,
-            name: c.name.common,
-            flag: c.flags[0],
-            continent: c.continents,
-            capital: c.capital,
-            subregion: c.subregion,
-            area: c.area,
-            population: c.population,
-          };
-        });
-        Country.bulkCreate(countriesDB).then(() => {
-          storedData = true;
-          next();
-        });
+    axios("https://restcountries.com/v3/all").then((res) => {
+      const countriesDB = res.data.map((c) => {
+        return {
+          id: c.cca3,
+          name: c.name.common,
+          flag: c.flags[0],
+          continent: c.continents[0],
+          capital: c.capital ? c.capital[0] : "No tiene",
+          subregion: c.subregion,
+          area: c.area,
+          population: c.population,
+        };
       });
+      Country.bulkCreate(countriesDB).then(() => {
+        storedData = true;
+        console.log("STOOOOOOOOOOOOOOOOOOORED");
+        next();
+      });
+    });
   } else next();
 }
 
 router.get("/", validateStoredData, async (req, res) => {
   const { name } = req.query;
+  let countries;
 
   try {
-    const countries = await Country.findAll({
-      where: name
-        ? {
-            name: { [Op.like]: `%${name}%` },
-          }
-        : undefined,
-    });
+    if (name) {
+      countries = await Country.findAll({
+        where: {
+          name: { [Op.like]: `%${name}%` },
+        },
+      });
+    } else {
+      countries = await Country.findAll();
+    }
+    // const countries = await Country.findAll({
+    //         where: name
+    //           ? {
+    //               name: { [Op.like]: `%${name}%` },
+    //             }
+    //           : null,
+    //       }
+    // );
 
     return res.status(200).json(countries.toJSON());
   } catch (e) {
-    return res
-      .status(404)
-      .send(
-        name
-          ? `No hubo resultados para "${name}"`
-          : "No se encontraron resultados"
-      );
+    return res.status(404).send(
+      name ? `No hubo resultados para "${name}"` : e //"No se encontraron resultados"
+    );
   }
 });
 
 router.get("/:id", (req, res) => {
   const { id } = req.params;
 
-  Country.findByPk(id,
-    { include: Activity })
-  .then((country) => {
+  Country.findByPk(id, { include: Activity })
+    .then((country) => {
       return res.status(200).json(country.toJSON());
     })
-  .catch((e) => {
-    return res.status(404).send(`No se encontró un país con ID ${id}`);
-  });
+    .catch((e) => {
+      return res.status(404).send(`No se encontró un país con ID ${id}`);
+    });
 });
-
-router.post("/", async (req, res)=>{
-  const {name, difficulty, duration, season} = req.body;
-
-  if (!name) return res.status(404).send("La actividad debe tener un nombre")
-
-  try {
-    
-  } catch (e) {
-    
-  }
-})
 
 module.exports = router;
